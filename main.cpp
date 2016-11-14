f996b2a65a008f71ca3f55005fc15aac9113dc39
Danilin Vsevolod <danilin_vsevolod@mail.ru>
2016-11-14 12:32:58 +0300

    Add method func() to Base and it's calling for two objects

--- main.cpp
+++ main.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
- using namespace std;
 
- class Base
+using namespace std;
+
+class Base
 {
- public:
-     Base() { cout << "Construct Base object\n"; }
+public:
+    Base() { cout << "Construct Base object\n"; }
      ~Base() { cout << "Destruct Base object\n"; }
-};
+
+     void func() { cout << "Function func() of class Base\n"; }
+ };
 
  class Child: public Base
-{
+ {
  public:
- Child() { cout << "Construct Child object\n"; }
+     Child() { cout << "Construct Child object\n"; }
      ~Child() { cout << "Destruct Child object\n"; }
  };
 
  int main(int argc, char *argv[])
  {
+     cout << "* Create Object base" << endl;
+     Base base;
+
      cout << "* Create Object child" << endl;
      Child child;
+
+     cout << "* Call method func() for object base" << endl;
+     base.func();
+
+     cout << "* Call method func() for object child" << endl;
+     child.func();
 
      return 0;
  }
