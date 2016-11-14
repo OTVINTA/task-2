19730ec9e8cb92556313ec8e1b128223d29ff09c
Danilin Vsevolod <danilin_vsevolod@mail.ru>
2016-11-14 12:44:27 +0300

    Add common Function() for constant refference to Base

--- main.cpp
+++ main.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
 
-using namespace std;
+ using namespace std;
 
-class Base
-{
-public:
-    Base() { cout << "Construct Base object\n"; }
-    ~Base() { cout << "Destruct Base object\n"; }
+ class Base
+ {
+ public:
+     Base() { cout << "Construct Base object\n"; }
+     ~Base() { cout << "Destruct Base object\n"; }
 
-    void func() { cout << "Function func() of class Base\n"; }
-};
-
-class Child: public Base
-{
-public:
-  Child() { cout << "Construct Child object\n"; }
-    ~Child() { cout << "Destruct Child object\n"; }
-
-     void func() { cout << "Function func() of class Child\n"; }
+     void func() const { cout << "Function func() of class Base\n"; }
  };
+
+ class Child: public Base
+ {
+ public:
+     Child() { cout << "Construct Child object\n"; }
+     ~Child() { cout << "Destruct Child object\n"; }
+
+     void func() const { cout << "Function func() of class Child\n"; }
+ };
+
+ void Function(const Base &obj)
+ {
+     cout << "* Call method func() for object obj" << endl;
+     obj.func();
+ }
 
  int main(int argc, char *argv[])
  {
@@ -29,10 +35,10 @@ public:
      Child child;
 
      cout << "* Call method func() for object base" << endl;
-     base.func();
+     Function(base);
 
      cout << "* Call method func() for object child" << endl;
-     child.func();
+     Function(child);
 
      return 0;
  }
