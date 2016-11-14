3085c1eae5e66c456108eaa77ddf5feb12e30c64
Danilin Vsevolod <danilin_vsevolod@mail.ru>
2016-11-14 12:38:32 +0300

    Overload method func() in Child

--- main.cpp
+++ main.cpp
@@ -6,16 +6,18 @@ class Base
 {
 public:
     Base() { cout << "Construct Base object\n"; }
-     ~Base() { cout << "Destruct Base object\n"; }
+    ~Base() { cout << "Destruct Base object\n"; }
 
-     void func() { cout << "Function func() of class Base\n"; }
- };
+    void func() { cout << "Function func() of class Base\n"; }
+};
 
- class Child: public Base
- {
- public:
-     Child() { cout << "Construct Child object\n"; }
-     ~Child() { cout << "Destruct Child object\n"; }
+class Child: public Base
+{
+public:
+  Child() { cout << "Construct Child object\n"; }
+    ~Child() { cout << "Destruct Child object\n"; }
+
+     void func() { cout << "Function func() of class Child\n"; }
  };
 
  int main(int argc, char *argv[])
